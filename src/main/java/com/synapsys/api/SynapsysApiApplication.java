8a343145cfd990e2b163d4628f2dd033f356ec84
Le-Gall-Valentin <valentin.legall55@gmail.com>
2026-05-06 22:54:21 +0200

    chore: integrate Spring Boot project with Maven wrapper configuration

---
+++ src/main/java/com/synapsys/api/SynapsysApiApplication.java
@@ -0,0 +1,13 @@
+package com.synapsys.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapsysApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynapsysApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/synapsys/api/SynapsysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synapsys.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapsysApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
