563f923c80f9ed2570ba994ed58fe87be06e1ffa
Le-Gall-Valentin <valentin.legall55@gmail.com>
2026-05-17 12:39:17 +0200

    feat(front/tooling): add eslint-plugin-boundaries to enforce FSD layer rules

--- src/main/frontend/eslint.config.js
+++ src/main/frontend/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import boundaries from 'eslint-plugin-boundaries'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -20,6 +21,46 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+  {
+    plugins: { boundaries },
+    settings: {
+      'boundaries/elements': [
+        { type: 'shared',   pattern: ['src/shared/**'] },
+        { type: 'entities', pattern: ['src/entities/**'] },
+        { type: 'features', pattern: ['src/features/*/**'], capture: ['feature'] },
+        { type: 'pages',    pattern: ['src/pages/*/**'],    capture: ['page'] },
+        { type: 'app',      pattern: ['src/app/**'] },
+      ],
+      'boundaries/ignore': ['src/main.tsx', 'src/vite-env.d.ts'],
+    },
+    rules: {
+      'boundaries/element-types': ['error', {
+        default: 'disallow',
+        rules: [
+          { from: 'shared',   allow: ['shared'] },
+          { from: 'entities', allow: ['shared', 'entities'] },
+          {
+            from: 'features',
+            allow: [
+              'shared',
+              'entities',
+              ['features', { feature: '${feature}' }],
+            ],
+          },
+          {
+            from: 'pages',
+            allow: [
+              'shared',
+              'entities',
+              'features',
+              ['pages', { page: '${page}' }],
+            ],
+          },
+          { from: 'app', allow: ['shared', 'entities', 'features', 'pages', 'app'] },
+        ],
+      }],
     },
   },
 )
