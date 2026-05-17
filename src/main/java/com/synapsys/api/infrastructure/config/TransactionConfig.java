e606d44e1939fa5309223643b1a7e154ec219b2f
Le-Gall-Valentin <valentin.legall55@gmail.com>
2026-05-17 12:24:47 +0200

    refactor(infra): simplify TransactionConfig to use AnnotationMatchingPointcut

--- src/main/java/com/synapsys/api/infrastructure/config/TransactionConfig.java
+++ src/main/java/com/synapsys/api/infrastructure/config/TransactionConfig.java
@@ -3,11 +3,9 @@ package com.synapsys.api.infrastructure.config;
 import com.synapsys.api.auth.domain.model.AuthException;
 import com.synapsys.api.shared.annotation.DomainService;
 import org.springframework.aop.Advisor;
-import org.springframework.aop.ClassFilter;
-import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
-import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.TransactionManager;
@@ -32,23 +30,7 @@ public class TransactionConfig {
         // All other methods: default transactional behavior (REQUIRED, rollback on any exception)
         source.addTransactionalMethod("*", new DefaultTransactionAttribute());
 
-        // Target only beans annotated with @DomainService
-        AspectJExpressionPointcut aspectJPointcut = new AspectJExpressionPointcut();
-        aspectJPointcut.setExpression("@target(com.synapsys.api.shared.annotation.DomainService)");
-
-        // Guard the class filter so Spring never tries to CGLIB-proxy classes it cannot subclass
-        Pointcut pointcut = new Pointcut() {
-            @Override
-            public ClassFilter getClassFilter() {
-                return clazz -> clazz.isAnnotationPresent(DomainService.class);
-            }
-
-            @Override
-            public MethodMatcher getMethodMatcher() {
-                return aspectJPointcut.getMethodMatcher();
-            }
-        };
-
+        Pointcut pointcut = AnnotationMatchingPointcut.forClassAnnotation(DomainService.class);
         return new DefaultPointcutAdvisor(pointcut, new TransactionInterceptor(txManager, source));
     }
 }
