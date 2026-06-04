dc3e643ff4262d3d8d62883a945ab8305fbafcac
Le-Gall-Valentin <valentin.legall55@gmail.com>
2026-06-04 23:39:56 +0200

    feat(totp): add disable method to totpApi and ITotpEnrollApi

--- src/main/frontend/src/features/totp/model/ITotpEnrollApi.ts
+++ src/main/frontend/src/features/totp/model/ITotpEnrollApi.ts
@@ -1,6 +1,8 @@
 import type { TotpSetupData } from './types'
+
 export interface ITotpEnrollApi {
   setup(): Promise<TotpSetupData>
   confirm(code: string): Promise<void>
   getStatus(): Promise<{ totpEnabled: boolean }>
+  disable(code: string): Promise<void>
 }
